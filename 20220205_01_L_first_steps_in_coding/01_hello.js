a598bdde48c89b6ea8ef27aded305db29e613e68
theterminal <kirilk777@gmail.com>
2023-01-09 12:50:48 +0200

    Init

---
+++ 20220205_01_L_first_steps_in_coding/01_hello.js
@@ -0,0 +1,8 @@
+// 20220205 - JavaScript - Programming Basics - First Steps in Coding
+// 01 - Hello Softuni - judge: https://judge.softuni.org/Contests/Compete/Index/2399#0
+
+
+function hello() {
+  console.log("Hello Matrix");
+}
+hello();
