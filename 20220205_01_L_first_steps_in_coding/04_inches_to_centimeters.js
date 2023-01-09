65169b8704f2de0b3e242c32cc334529db826511
theterminal <kirilk777@gmail.com>
2023-01-09 13:02:32 +0200

    Initial

---
+++ 20220205_01_L_first_steps_in_coding/04_inches_to_centimeters.js
@@ -0,0 +1,11 @@
+// 20220205 - JavaScript - Programming Basics - First Steps in Coding
+// 04 - Inches to Centimeters - judge: https://judge.softuni.org/Contests/Compete/Index/2399#3
+
+
+function InchesToCentimeters(input) {
+  let inches = Number(input[0]);
+  let centimeters = inches * 2.54;
+
+  console.log(centimeters);
+}
+InchesToCentimeters(["10"])
