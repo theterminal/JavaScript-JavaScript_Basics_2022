1bcee2461138d0b5f5df25493246f476deb14703
theterminal <kirilk777@gmail.com>
2023-01-09 13:04:59 +0200

    Initial

---
+++ 20220205_01_L_first_steps_in_coding/05_greetings_by_name.js
@@ -0,0 +1,12 @@
+// 20220205 - JavaScript - Programming Basics - First Steps in Coding
+// 05 - Greetings By Name - judge: https://judge.softuni.org/Contests/Compete/Index/2399#4
+
+
+function greetingsByName(input) {
+  let name = input[0];
+  let greeting = "Hello, " + name + "!";
+
+  console.log(greeting);
+}
+
+greetingsByName(["Kiril"])
