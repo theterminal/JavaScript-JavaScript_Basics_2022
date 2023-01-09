ef2b7e2e66c9719f28b84f1f156f1006bac2d3ff
theterminal <kirilk777@gmail.com>
2023-01-09 12:59:18 +0200

    Initial

---
+++ 20220205_01_L_first_steps_in_coding/03_square_area.js
@@ -0,0 +1,11 @@
+// 20220205 - JavaScript - Programming Basics - First Steps in Coding
+// 03 - Square Area - judge: https://judge.softuni.org/Contests/Compete/Index/2399#2
+
+
+function squareArea(input){
+  let side = Number(input[0]);
+  let area = side * side;
+  console.log(area);
+}
+
+squareArea(["5"]);
