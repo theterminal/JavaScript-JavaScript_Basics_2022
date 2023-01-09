730818be2775a2ea3c2f5365752c34637b9a685f
theterminal <kirilk777@gmail.com>
2023-01-09 12:55:42 +0200

    Init

---
+++ 20220205_01_L_first_steps_in_coding/02_numbers_1_to_10.js
@@ -0,0 +1,18 @@
+// 20220205 - JavaScript - Programming Basics - First Steps in Coding
+// 02 - Numbers from 1 to 10 - judge: https://judge.softuni.org/Contests/Compete/Index/2399#1
+
+
+function numbersFrom1To10() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
+  console.log(9);
+  console.log(10);
+}
+
+numbersFrom1To10()
