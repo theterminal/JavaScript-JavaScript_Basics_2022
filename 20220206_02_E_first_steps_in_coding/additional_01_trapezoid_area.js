06e39af906b09fbc22c79e9af7046f08430832a1
theterminal <kirilk777@gmail.com>
2023-01-10 08:44:11 +0200

    Initial

---
+++ 20220206_02_E_first_steps_in_coding/additional_01_trapezoid_area.js
@@ -0,0 +1,15 @@
+// 20220209 - JavaScript - Programming Basics - First Steps in Coding
+// Additional 01 - Trapezoid Area - judge: https://judge.softuni.org/Contests/Practice/Index/1642#0
+
+
+function trapeziodArea(input) {
+    let b1 = Number(input[0]);
+    let b2 = Number(input[1]);
+    let h = Number(input[2]);
+
+    let area = (b1 + b2) * h / 2;
+
+    console.log(area.toFixed(2));
+}
+
+trapeziodArea(["8", "13", "7"]);
