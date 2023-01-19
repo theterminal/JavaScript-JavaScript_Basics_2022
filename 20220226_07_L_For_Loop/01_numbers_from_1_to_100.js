b9d7f6ef0dc86afc627ee628fed4e7394c0d79ea
theterminal <kirilk777@gmail.com>
2023-01-19 14:21:01 +0200

    Initial

---
+++ 20220226_07_L_For_Loop/01_numbers_from_1_to_100.js
@@ -0,0 +1,11 @@
+// 20220228 - JavaScript - For Loops
+// 01 - Numbers From 1 To 100 - judge: https://judge.softuni.org/Contests/Compete/Index/2405#0
+
+
+function numbersFrom1To100() {
+    for (let i = 1; i <= 100; i++){
+        console.log(i);
+    }
+}
+
+numbersFrom1To100();
