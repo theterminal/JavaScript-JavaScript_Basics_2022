8341fa9d439e36f8a64dfa85598ef3643f1366dd
theterminal <kirilk777@gmail.com>
2023-01-19 13:57:47 +0200

    Initial

---
+++ 20220220_06_E_Nested Conditional Statements/additional_09_numbers_from_1_to_10.js
@@ -0,0 +1,23 @@
+// 20220222 JavaScript - Nested Conditional Statements
+// Additional 09 - Numbers From 1 To 10 - judge: https://judge.softuni.org/Contests/Practice/Index/1663#8
+
+
+function numbers1To10() {
+    let i = 1;
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+}
+numbers1To10();
+
+
+// ------------- done with "for" loop -----------------------
+
+
+function numbersFrom1To10() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i}`);
+    }
+}
+numbersFrom1To10();
