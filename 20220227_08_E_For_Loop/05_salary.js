6e9bd2d39fe5e95089eba8972b74954b96ab5192
theterminal <kirilk777@gmail.com>
2023-01-19 14:39:54 +0200

    Initial

---
+++ 20220227_08_E_For_Loop/05_salary.js
@@ -0,0 +1,32 @@
+// 20220302 - JavaScript - For Loops
+// 05 - Slaray - judge: https://judge.softuni.org/Contests/Compete/Index/2406#4
+
+
+function salary(input) {
+    let index = 0;
+    let numBrowsersOpened = Number(input[index++]);
+    let salary = Number(input[index++]);
+    let nameSite = ""; let hasSalary = true;
+
+    for (let i = 0; i < numBrowsersOpened; i++) {
+        nameSite = input[index++];
+
+        switch(nameSite) {
+            case "Facebook": salary -= 150; break;
+            case "Instagram": salary -= 100; break;
+            case "Reddit": salary -= 50; break;
+        }
+
+        if (salary <= 0) {
+            hasSalary = false;
+            console.log(`You have lost your salary.`);
+            break;
+        }
+    }
+    if (hasSalary) {
+        console.log(Math.trunc(salary));
+    } 
+}
+
+salary(["10", "750", "Facebook", "Dev.bg", "Instagram", "Facebook", "Reddit", "Facebook", "Facebook"]);
+salary(["3", "500", "Github.com", "Stackoverflow.com", "softuni.bg"]);
