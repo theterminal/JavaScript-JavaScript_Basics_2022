d4a10510b0c218195b0c9725bfb65232dcc20c5f
theterminal <kirilk777@gmail.com>
2023-01-25 08:46:57 +0200

    Initial

---
+++ 20220305_09_L_While_Loop/02_password.js
@@ -0,0 +1,21 @@
+// 20220303 - JavaScript - While Loop
+// 02 - Password - judge: https://judge.softuni.org/Contests/Compete/Index/2407#1
+
+
+function password(input) {
+    let index = 0;
+    let user = input[index++];
+    let password = input[index++];
+    let entry = input[index++];
+
+    while (entry !== password && index < Number(input.length)) {  // This way you cannot make an infinate loop
+        entry = input[index++];
+    }
+    if (entry === password) {
+        console.log(`Welcome ${user}!`);
+    } else {
+        console.log(`You didn't guess rigth!`)
+    } 
+}
+
+password(["Nakov", "1234", "Pass", "1324", "1234"]);
