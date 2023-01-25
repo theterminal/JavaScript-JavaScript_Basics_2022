bf925a647ebba4c8cca400af887f7816c6911c3d
theterminal <kirilk777@gmail.com>
2023-01-25 08:46:43 +0200

    Initial

---
+++ 20220305_09_L_While_Loop/01_read_text.js
@@ -0,0 +1,18 @@
+// 20220303 - JavaScript - While Loop
+// 01 - Read Text - judge: https://judge.softuni.org/Contests/Compete/Index/2407#0
+
+
+function readText(input) {
+    let index = 0;
+    
+    while (true) {
+        let str = input[index++];
+        if (str === "Stop") {
+            break;
+        }
+        console.log(str);
+    }
+}
+
+readText(["Nakov", "SoftUni", "Sofia", "Bulgaria", "SomeText", "Stop", "AfterStop", "Europe", "HelloWorld"]);
+readText(["Sofia", "Berlin", "Moscow", "Athens", "Madrid", "London", "Paris", "Stop", "AfterStop"]);
