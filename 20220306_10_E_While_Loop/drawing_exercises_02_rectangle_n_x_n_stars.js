2405ec8d5c76551d83ee30bf07e6997ac21a083a
theterminal <kirilk777@gmail.com>
2023-01-26 08:21:29 +0200

    Initial

---
+++ 20220306_10_E_While_Loop/drawing_exercises_02_rectangle_n_x_n_stars.js
@@ -0,0 +1,23 @@
+// 20220306 - JavaScript - While Loop
+// Drawing 02 - Rectangle N x N Stars - judge: https://judge.softuni.org/Contests/Practice/Index/1851#1
+
+
+function rectangleNXNStars(input) {
+    let index = 0;
+    let num = Number(input[index++]);
+    let result = ""; let counter = 0;
+
+    for (let i = 0; i < num; i++) {
+        if (counter === 0) {
+            for (let j = 0; j < num; j++) {
+                result = result + "*";
+                counter++;
+            }
+        }
+        console.log(result);
+    }
+}
+
+rectangleNXNStars(["2"]);
+rectangleNXNStars(["3"]);
+rectangleNXNStars(["10"]);
