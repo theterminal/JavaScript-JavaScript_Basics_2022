e241c2cd3dec7f30e1b3b2049b3f9a3cf7f70fe1
theterminal <kirilk777@gmail.com>
2023-01-28 08:54:41 +0200

    Initial

---
+++ 20220313_12_E_Nested_Loops/05_special_numbers.js
@@ -0,0 +1,28 @@
+// 20220310 JavaScript - Nested Loops
+// 05 - Special Numbers - judge: https://judge.softuni.org/Contests/Compete/Index/2410#4
+
+
+function specialNumbers(input) {
+    let index = 0;
+    let n = Number(input[index++]);
+    let output = "";
+
+    for (let i = 1111; i <= 9999; i++) {
+        let iString = i.toString();
+
+        for (let j = 0; j < 4; j++) {
+            let iStringIndexJ = iString[j];
+
+            if ((n % Number(iStringIndexJ)) != 0) {
+                break;
+            } else if (j === 3) {
+                output = `${output} ${iString}`;
+            }
+        }
+    }
+    console.log(output);
+}
+
+specialNumbers(["3"]);
+specialNumbers(["11"]);
+specialNumbers(["16"]);
