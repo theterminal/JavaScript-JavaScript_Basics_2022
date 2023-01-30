6696e566c517f95aaab1c4863f48152ac4306e2b
theterminal <kirilk777@gmail.com>
2023-01-30 08:23:50 +0200

    Initial

---
+++ 20220319_13_JavaScript_Exam_Prep_&_PreExam/20220319_JavaScript_Exam_Prep/examPrep_02_family_trip.js
@@ -0,0 +1,26 @@
+// 20220319 JavaScript - Exam Prep
+// 02 - Family Trip - judge: https://judge.softuni.bg/Contests/Practice/Index/1745#1
+
+
+function familyTrip(input) {
+    let index = 0;
+    let budget = Number(input[index++]);
+    let numNights = Number(input[index++]);
+    let pricePerNight = Number(input[index++]);
+    let percentAdditionalExpences = Number(input[index++]);
+
+    if (numNights > 7) {
+        pricePerNight *= 0.95;
+    }
+    let cost = numNights * pricePerNight + (budget * percentAdditionalExpences / 100);
+    let diff = Math.abs((budget - cost)).toFixed(2);
+
+    if (budget >= cost) {
+        console.log(`Ivanovi will be left with ${diff} leva after vacation.`);
+    } else {
+        console.log(`${diff} leva needed.`);
+    }
+}
+
+familyTrip(["800.50", "8", "100", "2"]);
+familyTrip(["500", "7", "66", "15"]);
