68f94a8caba419f8e78a010fa657ef6dccd56c0a
theterminal <kirilk777@gmail.com>
2023-01-31 14:23:36 +0200

    Initial

---
+++ 20220401_14_KK_scripts/20220226_todaysDay.js
@@ -0,0 +1,20 @@
+// 20220226 JavaScript - new Date().getDay()
+
+
+function todaysDay() {
+    let text = "";
+    
+    switch (new Date().getDay()) {
+        case 0: text = "Today is Sunday"; break;
+        case 1: text = "Today is Monday"; break;
+        case 2: text = "Today is Tuesday"; break;
+        case 3: text = "Today is Wednesday"; break;
+        case 4: text = "Today is Thursday"; break;
+        case 5: text = "Today is Friday"; break;
+        case 6: text = "Today is Saturday"; break;
+        default: text = "This default text should never be reached!"; break;
+    }
+  console.log(text);
+}
+
+todaysDay();
