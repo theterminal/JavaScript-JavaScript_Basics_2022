c20eff2dd4bad91689d08f68a91caca0483f7f85
theterminal <kirilk777@gmail.com>
2023-01-31 14:22:17 +0200

    Initial

---
+++ 20220319_13_JavaScript_Exam_Prep_&_PreExam/20220319_JavaScript_PreExam/JSPreExam_20220319_02_maiden_party.js
@@ -0,0 +1,38 @@
+// 20220319 JavaScript - Pre Exam
+// Pre Exam 02 - Maiden Party - judge: https://judge.softuni.org/Contests/Compete/Index/3386#1
+
+
+function maidenParty(input) {
+    let index = 0;
+    let costMaidenParty = Number(input[index++]);
+    let numLoveMesseges = Number(input[index++]);
+    let numWaxRoses = Number(input[index++]);
+    let numKeychains = Number(input[index++]);
+    let numCaricatures = Number(input[index++]);
+    let numSurprises = Number(input[index++]);
+
+    let moneyLoveMesseges = numLoveMesseges * 0.60;
+    let moneyWaxRoses = numWaxRoses * 7.20;
+    let moneyKeychains = numKeychains * 3.60;
+    let moneyCaricatures = numCaricatures * 18.20;
+    let moneySurprises = numSurprises * 22;
+
+    let totalMoney = moneyLoveMesseges + moneyWaxRoses + moneyKeychains + moneyCaricatures + moneySurprises;
+    let totalItemsOrdered = numLoveMesseges + numWaxRoses + numKeychains + numCaricatures + numSurprises;
+
+    if (totalItemsOrdered >= 25) {
+        totalMoney *= 0.65;
+    }
+    totalMoney *= 0.90;
+
+    let diff = Math.abs(totalMoney - costMaidenParty);
+
+    if (totalMoney >= costMaidenParty) {
+        console.log(`Yes! ${diff.toFixed(2)} lv left.`);
+    } else {
+        console.log(`Not enough money! ${diff.toFixed(2)} lv needed.`);
+    }
+}
+
+maidenParty(["40.8", "20", "25", "30", "50", "10"]);
+maidenParty(["320", "8", "2", "5", "5", "1"]);
