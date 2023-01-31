aa74d722f72c1df01621dfbecefdf20a9ffa8b50
theterminal <kirilk777@gmail.com>
2023-01-31 14:23:49 +0200

    Initial

---
+++ 20220401_14_KK_scripts/20220307_multiplicationTable.js
@@ -0,0 +1,16 @@
+// 20220307 JavaScript
+
+
+function multiplicationTable() {
+    let result = "";
+    for (let i = 1; i < 10; i++) {
+        for (let j = 1; j < 10; j++) {
+            product = i * j;
+            result += `${product}, `;
+        }
+        console.log(result);
+        result = "";
+    }
+}
+
+multiplicationTable();
