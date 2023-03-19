5ec44afd7bcd53d082bb8d93341413f816ccf986
theterminal <kirilk777@gmail.com>
2023-03-19 02:33:09 +0200

    Corrected spelling errors

--- 20220206_02_E_first_steps_in_coding/additional_06_fishland.js
+++ 20220206_02_E_first_steps_in_coding/additional_06_fishland.js
@@ -1,8 +1,8 @@
 // 20220209 - JavaScript - Programming Basics - First Steps in Coding
-// Additional 06 - Fishland - judge: https://judge.softuni.org/Contests/Practice/Index/1642#5
+// Additional 06 - Fish Land - judge: https://judge.softuni.org/Contests/Practice/Index/1642#5
 
 
-function fishland(input) {
+function fishLand(input) {
     let priceMackerelKg = Number(input[0]);
     let priceSpratKg = Number(input[1]);
     let kgBonito = Number(input[2]);
@@ -18,5 +18,5 @@ function fishland(input) {
     console.log(totalAmount.toFixed(2));
 }
 
-fishland(["6.90", "4.20", "1.5", "2.5", "1"]);
-fishland(["5.55", "3.57", "4.3", "3.6", "7"]);
+fishLand(["6.90", "4.20", "1.5", "2.5", "1"]);
+fishLand(["5.55", "3.57", "4.3", "3.6", "7"]);
