aac0f6aa07f84b2ec1981e270364a1e40bf6dcb4
theterminal <kirilk777@gmail.com>
2023-03-19 02:33:16 +0200

    Corrected spelling errors

--- 20220206_02_E_first_steps_in_coding/additional_10_wheather_forecast_2.js
+++ 20220206_02_E_first_steps_in_coding/additional_10_wheather_forecast_2.js
@@ -7,15 +7,15 @@ function weatherForecast2(input) {
 
     if (degrees >= 26.00 && degrees <= 35.00) {
         console.log("Hot");
-    }else if (degrees >= 20.1 && degrees <= 25.9) {
+    } else if (degrees >= 20.1 && degrees <= 25.9) {
         console.log("Warm");
-    }else if (degrees >= 15.00 && degrees <= 20.00) {
+    } else if (degrees >= 15.00 && degrees <= 20.00) {
         console.log("Mild");
-    }else if (degrees >= 12.00 && degrees <= 14.9) {
+    } else if (degrees >= 12.00 && degrees <= 14.9) {
         console.log("Cool");
-    }else if (degrees >= 5.00 && degrees <= 11.9) {
+    } else if (degrees >= 5.00 && degrees <= 11.9) {
             console.log("Cold");
-    }else{
+    } else {
         console.log("unknown");
     }
 }
