26f984ad91a685cf4bb6b7ae373b93ef53b8b963
theterminal <kirilk777@gmail.com>
2023-03-19 02:58:26 +0200

    Refactored

--- 20220227_08_E_For_Loop/02_multiplication_table.js
+++ 20220227_08_E_For_Loop/02_multiplication_table.js
@@ -2,12 +2,13 @@
 // 02 - Multiplication Table - judge: https://judge.softuni.org/Contests/Compete/Index/2406#1
 
 
-function multiplicationTable(input) {
-    let num = Number(input[0]);
+function multiplicationTable(number) {
+    let num = Number(number);
 
     for (let i = 1; i <= 10; i++) {
         console.log(`${i} * ${num} = ${num * i}`);
     }
 }
+
 
 multiplicationTable(["5"]);
