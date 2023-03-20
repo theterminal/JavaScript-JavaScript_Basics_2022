a91832c404ba2cd00208eaccd35780d6d1cd2e4d
theterminal <kirilk777@gmail.com>
2023-03-20 11:54:31 +0200

    Refactored

--- 20220312_11_L_Nested_Loops/05_traveling.js
+++ 20220312_11_L_Nested_Loops/05_traveling.js
@@ -13,6 +13,7 @@ function traveling(input) {
         if (destination === "End") {
             break;
         }
+
         budget = Number(input[index++]);
 
         while (true) {
@@ -27,6 +28,7 @@ function traveling(input) {
         }
     }
 }
+
 
 traveling(["Greece", "1000", "200", "200", "300", "100", "150", "240", "Spain", "1200", "300", "500", "193", "423", "End"]);
 traveling(["France", "2000", "300", "300", "200", "400", "190", "258", "360", "Portugal", "1450", "400", "400", "200", "300", "300", "Egypt", "1900", "1000", "280", "300", "500", "End"]);
