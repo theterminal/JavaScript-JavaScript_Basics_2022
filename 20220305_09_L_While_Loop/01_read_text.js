ce6ab73f52834a41b4a3c38903efce39c58a879e
theterminal <kirilk777@gmail.com>
2023-03-20 11:52:36 +0200

    Refactored

--- 20220305_09_L_While_Loop/01_read_text.js
+++ 20220305_09_L_While_Loop/01_read_text.js
@@ -7,12 +7,15 @@ function readText(input) {
     
     while (true) {
         let str = input[index++];
+
         if (str === "Stop") {
             break;
         }
+
         console.log(str);
     }
 }
+
 
 readText(["Nakov", "SoftUni", "Sofia", "Bulgaria", "SomeText", "Stop", "AfterStop", "Europe", "HelloWorld"]);
 readText(["Sofia", "Berlin", "Moscow", "Athens", "Madrid", "London", "Paris", "Stop", "AfterStop"]);
