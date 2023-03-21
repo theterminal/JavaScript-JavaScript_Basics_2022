e8a92689d4c70064b62beefa1564f19c33e757ec
theterminal <kirilk777@gmail.com>
2023-03-21 14:00:29 +0200

    Refactored

--- 20220319_13_JavaScript_Exam_Prep_&_PreExam/20220319_JavaScript_Exam_Prep/examPrep_04_movie_ratings.js
+++ 20220319_13_JavaScript_Exam_Prep_&_PreExam/20220319_JavaScript_Exam_Prep/examPrep_04_movie_ratings.js
@@ -19,20 +19,24 @@ function movieRatings(input) {
             movieNameMinRating = movieName;
             minRating = movieRating;
         }
+
         if (movieRating > maxRating) {
             maxRating = movieRating;
             movieNameMaxRating = movieName;
         }
+
         if (movieRating < minRating) {
             minRating = movieRating;
             movieNameMinRating = movieName;
         }
     }
+
     let averageRating = (sumRatings / numMovies).toFixed(1);
     console.log(`${movieNameMaxRating} is with highest rating: ${maxRating.toFixed(1)}`);
     console.log(`${movieNameMinRating} is with lowest rating: ${minRating.toFixed(1)}`);
     console.log(`Average rating: ${averageRating}`);
 }
+
 
 movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
 movieRatings(["3", "Interstellar", "8.5", "Dangal", "8.3", "Green Book", "8.2"]);
