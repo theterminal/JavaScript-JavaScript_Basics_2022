40c455eb8d7a1c96f0f54775c49f54c38ce68317
theterminal <kirilk777@gmail.com>
2023-03-22 12:40:50 +0200

    Refactored

--- 20220401_14_KK_scripts/20220328_SquareInsideRightangleTriangle.js
+++ 20220401_14_KK_scripts/20220328_SquareInsideRightangleTriangle.js
@@ -1,10 +1,10 @@
-// 20220328 - Area Of A Square Inside Of Rightangle Triangle
-// Square inscribed in a rightangle triangle.
-// The 2 sides ('a' and 'b') of the triangle are given numbers and they are NOT the hypotensiosis.
+// 20220328 - Area Of A Square Inside Of Right Angle Triangle
+// Square inscribed in a right angle triangle.
+// The 2 sides ('a' and 'b') of the triangle are given numbers and they are NOT the hypotenuse.
 // Calculate the area of the triangle.
 
 
-function areaOfSquareInsideRightangelTriangle(input) {
+function areaOfSquareInsideRightAngelTriangle(input) {
     let index = 0;
     let a = Number(input[index++]);
     let b = Number(input[index++]);
@@ -20,4 +20,4 @@ function areaOfSquareInsideRightangelTriangle(input) {
     console.log(`Area of Square as percent from Triangle = ${percentSquareFromTriangleArea.toFixed(2)}%`);
 }
 
-areaOfSquareInsideRightangelTriangle(["4", "5"]);
+areaOfSquareInsideRightAngelTriangle(["4", "5"]);
